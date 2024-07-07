9cc8987d9b9efb8276a0428364d0486464390003
incognito1025 <nkhanamart@gmail.com>
2024-07-07 18:05:14 -0400

    updated app.js with middleware to parse(analyze data from transaction.js); added show route in transactionControllers to extract `id` from array in transation.js; added Index Route, more specifically catch block to display error message if something is off.

--- app.js
+++ app.js
@@ -4,18 +4,24 @@ const express = require("express")
 // Configuration
 const app = express()
 
+//Middleware - functions that have access to the request object (req), the response object(res) and middleware functions in request-response cycle
+app.use(express.json()) //method to mount middleware functions at a specific path. if no path, then the middleware is mounted to the root path (executed for every incoming request)
+//express.json parses incoming requests and handles JSON data
+
 //Controllers
 const transactionsController = require("./controllers/transactionsController")
 
 app.use("/transactions", transactionsController) //app.use("/transactions", transactionsController) - this will set up a middleware to handle routes under the /`transactions` path. transactionsController is passed as a middleware function to handle requests that strat with `/transactions`. This controller will help define routes and respective logic for CRUD operations (Create, Read, Update, Delete) on transactions.
 
 
-
-
 // Root/Health Check Route
-app.get("/", (req, res) => {
-    res.status(200).send("Welcome to Bank Transaction App")
+app.get("/", (request, response) => {
+    res.status(200).send("Welcome to Bank Transaction App") //Root route - app.get("/", ...) defines a route handler for GET requests to the root path ("/"). When a GET request is made to /, the callback function (req, res) => {...} is executed. Inside the callback function, res.status(200).send("Welcome to My Bank Transaction App") sends a HTTP status code of 200 (OK) along with the message "Welcome to My Bank Transaction App" as the response body.
 })
+
+
+
+
 
 // Export App
 module.exports = app
@@ -23,7 +29,7 @@ module.exports = app
 
 /*
 
-
+updated app.js with middleware to parse(analyze data); added show route in transactionControllers to extract `id` from array in transition.js; added 
 
 
 */
--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -5,9 +5,33 @@ const transactionsRouter = express.Router()
 const transactionArr = require("../models/transaction")
 
 // Index Route
-transactionsRouter.get("/", (req, res) => {
-    res.status(200).send(transactionArr)
+transactionsRouter.get("/", (request, response) => {
+    response.status(200).send(transactionArr)
+    try{  //the try catch block will handle errors that might occur when sendign the response
+        response.status(200).send(transactionArr) //send a response with status code sends the transactions.js array as the response body.
+    }
+    catch(error) {
+        response.status(404).json({error: `Something is off!`}) //if there is an error hopefully this message will flag it.
+    }
 })
+
+
+// Show Route
+transactionsRouter.get("/:id", (request, response) => { //defines route handle for GET request with paramater of `:id`
+
+    const { id } = request.params  //extracting parameters: destructure id from request param which holds parameters in the URL path (:id)
+
+    const transaction = transactionArr.find((transaction) => transaction.id === id) // transactionArr represents array in transaction.js. the find rearches for a transaction where the `transaction.id` matches the `id` extracted from the URL
+
+    if (transaction) {
+            response.status(200).send(transaction)
+    } else {
+        // res.status(404).json({error: `Transaction with id ${id} does not exist.`})
+        response.send("There is no transaction matching this id: " + id)
+    }
+})
+
+
 
 // Export
 module.exports = transactionsRouter;
